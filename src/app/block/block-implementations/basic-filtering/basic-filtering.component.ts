95b13c2b4fbd414dda65fcac4300754c3314d995
Andrew Laing <2775878l@student.gla.ac.uk>
2024-11-15 01:05:07 +0000

    better fleshed out some blocks

--- src/app/block/block-implementations/basic-filtering/basic-filtering.component.ts
+++ src/app/block/block-implementations/basic-filtering/basic-filtering.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { BlockComponent } from '../../block.component';
 import {NgStyle} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-filtering',
   standalone: true,
   imports: [
-    NgStyle
+    NgStyle,
+    FormsModule
   ],
   templateUrl: './basic-filtering.component.html',
   styleUrl: '../../block.component.css'
 })
 export class BasicFilteringComponent extends BlockComponent{
-  height: number = 60;
-  width: number = 150;
+  height: number = 120;
+  width: number = 180;
   color: string = "skyblue";
+
+  minimumGenesPerCell: number = 200;
+  minimumCellsPerGene: number = 3;
+  genesPerCell: number = this.minimumGenesPerCell;
+  cellsPerGene: number = this.minimumCellsPerGene;
 }
--- src/app/block/block-implementations/load-data/load-data.component.ts
+++ src/app/block/block-implementations/load-data/load-data.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { BlockComponent } from '../../block.component';
 import {NgStyle} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-filtering',
   standalone: true,
   imports: [
-    NgStyle
+    NgStyle,
+    FormsModule
   ],
   templateUrl: './load-data.component.html',
   styleUrl: '../../block.component.css'
 })
 export class LoadData extends BlockComponent{
-  height: number = 55;
+  height: number = 75;
   width: number = 200;
   color: string = "yellow";
+
+  selectedDataSet: string = "";
 }
